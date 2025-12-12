e3a4105b8ea61f86fb2eab4e37bae2a591502b7c
smallpookie <frankn14@nycstudents.net>
2025-12-11 22:02:17 -0500

    Extend Student model with email, GPA, and imageUrl fields

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+## Group Members
+
+- Jose Nocelo  
+  GitHub: https://github.com/nocelojose
+  Username: nocelojose
+
 # server-starter-code
 
 This repository is the server (back-end) starter code for Final Project - Full-Stack CRUD Application.
--- database/models/Student.js
+++ database/models/Student.js
@@ -15,6 +15,34 @@ const Student = db.define("student", {
   lastname: {
     type: Sequelize.STRING,
     allowNull: false
+  },
+
+  // Optional email (we'll require it in the front-end form)
+  email: {
+    type: Sequelize.STRING,
+    allowNull: true,
+    validate: {
+      isEmail: true
+    }
+  },
+
+  // Optional imageUrl (we'll treat it as optional in the form)
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: true,
+    validate: {
+      isUrl: true
+    }
+  },
+
+  // Optional GPA between 0.0 and 4.0
+  gpa: {
+    type: Sequelize.FLOAT,
+    allowNull: true,
+    validate: {
+      min: 0.0,
+      max: 4.0
+    }
   }
 });
 
